bbe2f2fe822cbc61f153691b1be4cec8211e1aa3
yashvid17-pixel <yashvid17@gmail.com>
2026-03-22 10:11:50 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+i  = int(input("enter a number: "))
+if (i<15):
+    print("i is smaller than 15")
+    print("i'm in if block")
+else:
+    print ("i is greater than 15 ")
+    print("i'm in else block")
+print ("i'm not in if and not in else block")
